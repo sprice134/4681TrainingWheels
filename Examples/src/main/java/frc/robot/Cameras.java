34ab0595ca6ea9f7681b2058fc6781362671dbdf
sprice134 <46632787+sprice134@users.noreply.github.com>
2020-03-14 13:11:03 -0700

    Moved Cameras to their own class

--- Examples/src/main/java/frc/robot/Cameras.java
+++ Examples/src/main/java/frc/robot/Cameras.java
@@ -3,7 +3,9 @@ package frc.robot;//Mandatory for all files
 import edu.wpi.first.cameraserver.CameraServer;//Required for cameras
 
 public class Cameras{
-
+    //There are more methods you can do with cameras but at the level this team is at, I do not recommend it
+    //Run the startAutomaticCapture method and that is all you will need for the entire match
+    //once that method is called, live camera feed will be accessible by both the driver station and smart dashboard
     public void camerasInit(){
         CameraServer.getInstance().startAutomaticCapture("Front", 0);//0,1 refer to usb ports on usb. Camera is now accessible in driverstation
         CameraServer.getInstance().startAutomaticCapture("Back", 1);//String is whatever the name is, change to whatever you want
